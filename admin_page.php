73d0848c68c04e600b595e3106fd4a6c87420b89
Nurul2Amin <106983417+Nurul2Amin@users.noreply.github.com>
2023-09-01 14:48:47 +0600

    admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -42,10 +42,10 @@ if(!isset($admin_id)){
       <div class="box">
          <?php
             $total_pendings = 0;
-            $select_pending = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payment_status = 'pending'") or die('query failed');
+            $select_pending = mysqli_query($conn, "SELECT total FROM `orders` WHERE paystatus = 'pending'") or die('query failed');
             if(mysqli_num_rows($select_pending) > 0){
                while($fetch_pendings = mysqli_fetch_assoc($select_pending)){
-                  $total_price = $fetch_pendings['total_price'];
+                  $total_price = $fetch_pendings['total'];
                   $total_pendings += $total_price;
                };
             };
@@ -57,10 +57,10 @@ if(!isset($admin_id)){
       <div class="box">
          <?php
             $total_completed = 0;
-            $select_completed = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payment_status = 'completed'") or die('query failed');
+            $select_completed = mysqli_query($conn, "SELECT total FROM `orders` WHERE paystatus = 'completed'") or die('query failed');
             if(mysqli_num_rows($select_completed) > 0){
                while($fetch_completed = mysqli_fetch_assoc($select_completed)){
-                  $total_price = $fetch_completed['total_price'];
+                  $total_price = $fetch_completed['total'];
                   $total_completed += $total_price;
                };
             };
@@ -89,7 +89,7 @@ if(!isset($admin_id)){
 
       <div class="box">
          <?php 
-            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE type = 'user'") or die('query failed');
             $number_of_users = mysqli_num_rows($select_users);
          ?>
          <h3><?php echo $number_of_users; ?></h3>
@@ -98,7 +98,7 @@ if(!isset($admin_id)){
 
       <div class="box">
          <?php 
-            $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
+            $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE type = 'admin'") or die('query failed');
             $number_of_admins = mysqli_num_rows($select_admins);
          ?>
          <h3><?php echo $number_of_admins; ?></h3>
@@ -112,15 +112,6 @@ if(!isset($admin_id)){
          ?>
          <h3><?php echo $number_of_account; ?></h3>
          <p>total accounts</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
-            $number_of_messages = mysqli_num_rows($select_messages);
-         ?>
-         <h3><?php echo $number_of_messages; ?></h3>
-         <p>new messages</p>
       </div>
 
    </div>
@@ -138,7 +129,7 @@ if(!isset($admin_id)){
 
 
 <!-- custom admin js file link  -->
-<script src="js/admin_script.js"></script>
+<script src="admin_script.js"></script>
 
 </body>
 </html>
